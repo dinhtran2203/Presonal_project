2ae1fba87128025c85715b92161a506012c11793
LoneWolf <dinh2203@gmail.com>
2021-07-16 16:24:19 -0700

    Hangman_Game

---
+++ Hangman/Hangman_game.py
@@ -0,0 +1,51 @@
+
+
+import random
+import string
+def get_random_word():
+	word = random.choice(open("word.txt").readlines())
+
+	"""
+	word.rstrip() is remove the last character in the string. 
+	the word in txt file is ***/n
+	"""
+	return word.rstrip().upper()
+
+def hang_man():
+
+	word = get_random_word()
+
+	# define unique words
+	hidden_word = set(word)
+
+	# alphabet character
+	alphabet = set(string.ascii_uppercase)
+
+	used_letters = set()
+
+	while len(hidden_word) > 0:
+
+		print()
+		# letter used
+		print("You have used: " , " ".join(used_letters))
+
+		# what current word is (ie w-s-d)
+		currentword = [letter if letter in used_letters else '-' for letter in word]
+		print("current word: ", " ".join(currentword))
+
+		userinput = input("Guess a letter: ").upper()
+		if userinput in alphabet - used_letters:
+			used_letters.add(userinput)
+			if userinput in hidden_word:
+				hidden_word.remove(userinput)
+		elif userinput in used_letters:
+			print("you have used that character. Please try again")
+		else:
+			print("Invalid character. Please try again")
+	print("---------------------------------")
+	print("Congratulation you found the word")
+	print("The word is: ", word)
+hang_man()
+
+
+
